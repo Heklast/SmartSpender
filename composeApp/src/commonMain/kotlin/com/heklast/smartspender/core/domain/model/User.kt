ace98de327bac02e0e77b7ebb2d42a8beb4d5e9c
Hekla <heklast@gmail.com>
2025-10-15 08:33:31 +0200

    user model

---
+++ composeApp/src/commonMain/kotlin/com/heklast/smartspender/core/domain/model/User.kt
@@ -0,0 +1,10 @@
+package com.heklast.smartspender.core.domain.model
+
+data class User(
+    val fullName: String,
+    val email: String,
+    val number: Int,
+    val birthDate: String
+    //pw?
+
+)
